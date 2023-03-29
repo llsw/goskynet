6f3fbd5eff6c5dea1977d2589041e3399436daf6
xmk <xmk@mac.com>
2023-03-29 22:49:42 +0800

    整理代码

--- service/service.go
+++ service/service.go
@@ -10,7 +10,7 @@ import (
 	"github.com/cloudwego/hertz/pkg/common/hlog"
 )
 
-var lock = &sync.Mutex{}
+var onceIns sync.Once
 
 type Method struct {
 	rcvr   reflect.Value
@@ -27,10 +27,8 @@ func (m *Method) call(req ...interface{}) (resp []interface{}, err error) {
 				err = fmt.Errorf("%s", v)
 
 			}
-			// hlog.Errorf("call error:%s", err.Error())
 		}
 	}()
-	// panic("test panic")
 	actually := len(req)
 	num := m.method.Type.NumIn()
 	in := make([]reflect.Value, actually+1)
@@ -98,15 +96,15 @@ var singleInstance *Service
 
 func GetInstance() *Service {
 	if singleInstance == nil {
-		lock.Lock()
-		defer lock.Unlock()
-		if singleInstance == nil {
-			singleInstance = &Service{
-				actors: make(map[string]*ActorGroup),
-				names:  make(map[*actor.PID]string),
-				system: actor.NewActorSystem(),
+		onceIns.Do(func() {
+			if singleInstance == nil {
+				singleInstance = &Service{
+					actors: make(map[string]*ActorGroup),
+					names:  make(map[*actor.PID]string),
+					system: actor.NewActorSystem(),
+				}
 			}
-		}
+		})
 	}
 	return singleInstance
 }
@@ -126,7 +124,8 @@ func doRegister(pid string, rcvr reflect.Value, typ reflect.Type) error {
 	return nil
 }
 
-func CallMethod(pid string, cmd string, args ...interface{}) (resp []interface{}, err error) {
+func CallMethod(pid string, cmd string,
+	args ...interface{}) (resp []interface{}, err error) {
 	if _, ok := methods[pid]; !ok {
 		err = fmt.Errorf("service:%s cmd:%s not found", pid, cmd)
 		return
@@ -153,7 +152,8 @@ func Register(pid string, receiver interface{}) error {
 	return nil
 }
 
-func (s *Service) newService(name string, service AcotrService) (pid *actor.PID, err error) {
+func (s *Service) newService(name string,
+	service AcotrService) (pid *actor.PID, err error) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
@@ -200,7 +200,8 @@ func (s *Service) newService(name string, service AcotrService) (pid *actor.PID,
 	return
 }
 
-func (s *Service) NewService(name string, service AcotrService) (pid *actor.PID, err error) {
+func (s *Service) NewService(name string,
+	service AcotrService) (pid *actor.PID, err error) {
 	if name == "cluster" {
 		err = fmt.Errorf("can not use cluster by service name")
 		return
@@ -208,7 +209,52 @@ func (s *Service) NewService(name string, service AcotrService) (pid *actor.PID,
 	return s.newService(name, service)
 }
 
-func (s *Service) Call(pidOrName interface{}, cmd string, args ...interface{}) (interface{}, error) {
+func (s *Service) callByPid(ctx *actor.RootContext,
+	pid *actor.PID, cmd string, message *Msg) (interface{}, error) {
+	resp, err := ctx.RequestFuture(pid, message, 30*time.Second).Result()
+	switch rt := resp.(type) {
+	// 集群那边的报错
+	case []interface{}:
+		lrt := len(rt)
+		if lrt > 0 {
+			switch v := rt[lrt-1].(type) {
+			case error:
+				err = v
+			}
+		}
+	case interface{}:
+		switch v := rt.(type) {
+		case error:
+			err = v
+		}
+	}
+	if err != nil {
+		if cmd != "Call" {
+			hlog.Errorf(
+				"sercice call:%v cmd:%s error:%s",
+				s.names[pid], cmd, err.Error())
+		}
+	}
+	return resp, err
+}
+
+func (s *Service) callByName(ctx *actor.RootContext,
+	name string, cmd string, message *Msg) (interface{}, error) {
+	if ag, ok := s.actors[name]; ok {
+		ag.Balance = (ag.Balance + 1) % len(ag.Actors)
+		pid := ag.Actors[ag.Balance]
+		if pid == nil {
+			return nil, fmt.Errorf(
+				"call service:%s not found", name)
+		}
+		return s.callByPid(ctx, pid, cmd, message)
+	} else {
+		return nil, fmt.Errorf("call service:%s not found", name)
+	}
+}
+
+func (s *Service) Call(pidOrName interface{},
+	cmd string, args ...interface{}) (interface{}, error) {
 	message := &Msg{
 		Cmd:  cmd,
 		Args: args,
@@ -216,61 +262,31 @@ func (s *Service) Call(pidOrName interface{}, cmd string, args ...interface{}) (
 	ctx := s.system.Root
 	switch v := pidOrName.(type) {
 	case string:
-		if ag, ok := s.actors[v]; ok {
-			ag.Balance = (ag.Balance + 1) % len(ag.Actors)
-			act := ag.Actors[ag.Balance]
-			if act == nil {
-				return nil, fmt.Errorf(
-					"call service:%s not found", pidOrName)
-			}
-			resp, err := ctx.RequestFuture(
-				act, message, 30*time.Second).Result()
-
-			switch rv := resp.(type) {
-			// 集群那边的报错
-			case []interface{}:
-				lv := len(rv)
-				if lv > 0 {
-					switch vv := rv[lv-1].(type) {
-					case error:
-						err = vv
-					}
-				}
-			case interface{}:
-				switch vv := rv.(type) {
-				case error:
-					err = vv
-				}
-			}
-			if err != nil {
-				if cmd != "Call" {
-					hlog.Errorf(
-						"sercice call:%v cmd:%s error:%s",
-						pidOrName, cmd, err.Error(),
-					)
-				}
-			}
-			return resp, err
-		} else {
-			return nil, fmt.Errorf("call service:%s not found", pidOrName)
-		}
+		return s.callByName(ctx, v, cmd, message)
 	case *actor.PID:
-		resp, err := ctx.RequestFuture(v, message, 30*time.Second).Result()
-		if err != nil {
-			return nil, err
-		}
-
-		switch v := resp.(type) {
-		case error:
-			return nil, v
-		default:
-			return resp, nil
-		}
+		return s.callByPid(ctx, v, cmd, message)
 	}
 	return nil, fmt.Errorf("call pidOrName type:%v invalid", pidOrName)
 }
 
-func (s *Service) Send(pidOrName interface{}, cmd string, args ...interface{}) (err error) {
+func (s *Service) sendByName(ctx *actor.RootContext,
+	name string, cmd string, message *Msg) error {
+	if ag, ok := s.actors[name]; ok {
+		ag.Balance = (ag.Balance + 1) % len(ag.Actors)
+		pid := ag.Actors[ag.Balance]
+		if pid == nil {
+			return fmt.Errorf(
+				"call service:%s not found", name)
+		}
+		ctx.Request(pid, message)
+		return nil
+	} else {
+		return fmt.Errorf("call service:%s not found", name)
+	}
+}
+
+func (s *Service) Send(pidOrName interface{},
+	cmd string, args ...interface{}) (err error) {
 	message := &Msg{
 		Cmd:  cmd,
 		Args: args,
@@ -278,18 +294,7 @@ func (s *Service) Send(pidOrName interface{}, cmd string, args ...interface{}) (
 	ctx := s.system.Root
 	switch v := pidOrName.(type) {
 	case string:
-		if ag, ok := s.actors[v]; ok {
-			ag.Balance = (ag.Balance + 1) % len(s.actors)
-			act := ag.Actors[ag.Balance]
-			if act == nil {
-				err = fmt.Errorf("send service:%s not found", pidOrName)
-				return
-			}
-			ctx.Request(act, message)
-		} else {
-			err = fmt.Errorf("send service:%s not found", pidOrName)
-		}
-		return
+		return s.sendByName(ctx, v, cmd, message)
 	case *actor.PID:
 		ctx.Request(v, message)
 	default:
@@ -329,10 +334,6 @@ func Receive(ctx actor.Context) {
 	case *Msg:
 		resp, err := CallMethod(ctx.Self().Id, msg.Cmd, msg.Args...)
 		if err != nil {
-			// hlog.Errorf(
-			// 	"pid:%s receive msg call method:%s err:%s",
-			// 	ctx.Self().Id, msg.Cmd, err.Error(),
-			// )
 			ctx.Respond(err)
 		} else {
 			ctx.Respond(resp)
